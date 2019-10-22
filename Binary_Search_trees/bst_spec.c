247976bc0e5b726e30194747e6d5c4d69f690107
shettypriya-495 <shettypriya495@gmail.com>
2019-10-22 12:53:30 +0530

    Update bst_spec.c

--- Binary_Search_trees/bst_spec.c
+++ Binary_Search_trees/bst_spec.c
@@ -4,15 +4,29 @@ void test_empty_bst()
 {
   BST b=bst_new();
   BST *tree=&b;
+  assert(tree->mass==0);
   
 }
-void test_three_leve_bst()
+void test_three_level_bst()
 {
+  BST b=bst_new();
+  BST *tree=&b;
 
+  tree=bst_add_node(tree,50);
+  assert(bst_count(tree)==1);
+ // tree=bst_delete_node(tree,50);
+  tree=bst_add_node(tree,30);
+  
+  tree=bst_add_node(tree,60);
+  tree=bst_add_node(tree,70);
+  assert(bst_count(tree)==4);
+  assert(bst_lookup(tree,60)==1);
+  tree=bst_in_order(tree);
+  
 }
 int main()
 {
 	test_empty_bst();
-	test_three_leve_bst();
+	test_three_level_bst();
 	return 0;
 }
