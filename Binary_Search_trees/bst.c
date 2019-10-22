9fdc8e1865ea7e830be9002e3e8ffa21e26bd512
shettypriya-495 <shettypriya495@gmail.com>
2019-10-22 12:51:51 +0530

    Update bst.c

--- Binary_Search_trees/bst.c
+++ Binary_Search_trees/bst.c
@@ -15,7 +15,7 @@ return btree;
 
 static Treenode* _bst_newnode(int32_t ele)
 {
-	Treenode *node=(Treenode*) malloc(sizeof(Treenode));
+	Treenode *node=(Treenode* ) malloc(sizeof(Treenode));
 	node->data=ele;
 	node->left=NULL;
 	node->right=NULL;
@@ -27,6 +27,7 @@ uint32_t bst_count(BST *tree)
 	assert(tree!=NULL);
 	return tree->mass;
 }
+
 BST* bst_add_node(BST *tree, int32_t element)
 {
 	assert(tree!=NULL);
@@ -57,7 +58,7 @@ BST* bst_add_node(BST *tree, int32_t element)
 		}
 		else if(element>parent->data)
 		{
-			parent=>right=tnode;
+			parent->right=tnode;
 		}
 		++tree->mass;
 	}
@@ -66,13 +67,61 @@ BST* bst_add_node(BST *tree, int32_t element)
 
 BST* bst_delete_node(BST *tree, int32_t key)
 {
-
-
-
-
-
+	assert(tree!=NULL);
+	tree -> root=delete(tree,tree->root,key);
+	return tree;
 }
-uint32_t bst_lookup(BST *tree)
+
+static Treenode* delete(BST *tree, Treenode *node, int32_t key)
+{
+	Treenode *temp;
+	if(node ==NULL)
+	{
+		return node;
+	}
+	else if(key< node->data)
+	{
+		node->left=delete(tree,node->left,key);
+	}
+	else if(key > node->data)
+	{
+		node->right=delete(tree,node->right,key);
+	}
+	else if(node->left && node->right)
+	{
+		temp=findmin(node->right);
+		node->data=temp->data;
+		node->right=delete(tree,node->right,node->data);
+
+	}
+	else
+	{
+		temp=node;
+		if(node->right==NULL)
+		{
+			node=node->left;
+		}
+		else
+		{
+			node=node->right;
+		}
+		free(temp);
+		--tree->mass;
+	}
+	return node;
+}
+
+static Treenode* findmin(Treenode *node)
+{
+	if(node->left ==NULL)
+	{
+		return node;
+	}
+	else
+		return(findmin(node->left));
+}
+
+uint32_t bst_lookup(BST *tree, int32_t key)
 {
 	assert(tree!=NULL);
 	Treenode *cur=tree->root;
@@ -82,55 +131,58 @@ uint32_t bst_lookup(BST *tree)
 		{
 			cur=cur->left;
 		}
-		else if(element>cur->data)
+		else if(key > cur->data)
 		{
 			cur=cur->right;
 		}
 	}
 	return (cur!=NULL);
 }
-BST* bst_inorder(BST *tree)
+
+BST* bst_in_order(BST *tree)
 {
 	assert(tree!=NULL);
-	_inorder_(tree->root);
+	_in_order_(tree->root);
 }
-static void _inorder_(Treenode *node)
+static void _in_order_(Treenode *node)
 {
 	if(node)
 	{
-		_inorder_(node->left);
+		_in_order_(node->left);
 		printf("%d\t", node->data );
-		_inorder_(node->left);
+		_in_order_(node->left);
 	}
 }
-BST* bst_postorder(BST *tree)
+BST* bst_post_order(BST *tree)
 {
 	assert(tree!=NULL);
-	_postorder_(tree->root);
+	_post_order_(tree->root);
 }
-static void _postorder_(Treenode *node)
+static void _post_order_(Treenode *node)
 {
 	if(node)
 	{
-		_postorder_(node->left);
-		_postorder_(node->left);
+		_post_order_(node->left);
+		_post_order_(node->left);
 		printf("%d\t", node->data );
 	}
 }
-BST* bst_preorder(BST *tree)
+
+BST* bst_pre_order(BST *tree)
 {
 	assert(tree!=NULL);
-	_preorder_(tree->root);
+	_pre_order_(tree->root);
 }
-static void _preorder_(Treenode *node)
+static void _pre_order_(Treenode *node)
 {
 	if(node)
 	{
 		printf("%d\t", node->data );
-		_preorder_(node->left);
-		_preorder_(node->left);
+		_pre_order_(node->left);
+		_pre_order_(node->left);
 	}
 }
+/*
 uint32_t bst_hieght(BST *tree)
 {
 
@@ -162,3 +214,4 @@ BST* bst_level_order(BST *tree)
 	printf("%d\t", node->data);
 	}
 }
+*/
